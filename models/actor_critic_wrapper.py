c36fa2318971901f46a00be4be1f53a544f9f50f
dtracers <dtracers@gmail.com>
2017-11-26 11:44:56 -0800

    made saltie an even more general model runner

---
+++ models/actor_critic_wrapper.py
@@ -0,0 +1,58 @@
+from actorcritic import PolicyGradientActorCritic
+
+class ActorCriticModel:
+
+    def __init__(self, session,
+                 state_dim,
+                 num_actions,
+                 optimizer=tf.train.AdamOptimizer(learning_rate=1e-4),
+                 summary_writer=None,
+                 summary_every=100):
+
+        self.session = session
+        self.num_actions = num_actions
+        self.state_dim = state_dim
+        self.summary_writer = summary_writer
+        self.summary_every = summary_every
+        self.pg_reinforce = PolicyGradientActorCritic(session,
+                                                      optimizer,
+                                                      self.actor_network,
+                                                      self.critic_network,
+                                                      self.state_dim,
+                                                      self.num_actions,
+                                                      summary_writer=summary_writer)
+
+    def actor_network(self, states):
+        # define policy neural network
+        W1 = tf.get_variable("W1", [self.state_dim, 20],
+                             initializer=tf.random_normal_initializer())
+        b1 = tf.get_variable("b1", [20],
+                             initializer=tf.constant_initializer(0))
+        h1 = tf.nn.tanh(tf.matmul(states, W1) + b1)
+        W2 = tf.get_variable("W2", [20, self.num_actions],
+                             initializer=tf.random_normal_initializer(stddev=0.1))
+        b2 = tf.get_variable("b2", [self.num_actions],
+                             initializer=tf.constant_initializer(0))
+        p = tf.matmul(h1, W2) + b2
+        return p
+
+    def critic_network(self, states):
+        # define policy neural network
+        W1 = tf.get_variable("W1", [self.state_dim, 20],
+                             initializer=tf.random_normal_initializer())
+        b1 = tf.get_variable("b1", [20],
+                             initializer=tf.constant_initializer(0))
+        h1 = tf.nn.tanh(tf.matmul(states, W1) + b1)
+        W2 = tf.get_variable("W2", [20, 1],
+                             initializer=tf.random_normal_initializer())
+        b2 = tf.get_variable("b2", [1],
+                             initializer=tf.constant_initializer(0))
+        v = tf.matmul(h1, W2) + b2
+        return v
+
+    def store_rollout(self, state, last_action, reward):
+        self.pg_reinforce.store_rollout(state, last_action, reward)
+
+
+    def sample_action(self, states):
+        self.pg_reinforce.sampleAction(states)
---
+++ models/empty_model_example.py
@@ -0,0 +1,17 @@
+class EmptyModelExample(object):
+    """"
+    This will be the example model framework with the needed functions but none of the code inside them
+    You can copy this to implement your own model
+    """
+    def __init__(self, session,
+                 num_actions,
+                 state_dim,
+                 summary_writer=None,
+                 summary_every=100):
+        print('i do nothing!')
+
+    def store_rollout(self, state, last_action, reward):
+        print(' i do nothing!')
+
+    def sample_action(self, states):
+        print(' i do nothing!')
