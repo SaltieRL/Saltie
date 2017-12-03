539e785658b9c4722bab4a717ec17d9a83699883
M <sciguymjm@gmail.com>
2017-12-03 12:04:17 -0800

    added batch file downloading from server for offline training

--- trainer/offline_trainer.py
+++ trainer/offline_trainer.py
@@ -1,18 +1,38 @@
 import gzip
-import os
-import time
-import sys
 import inspect
+import io
+import os
+import random
+import sys
+import time
+
+import requests
+
+import config
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+sys.path.insert(0, parentdir)
 
 from conversions import binary_converter
-from trainer import reward_trainer
 from trainer.model_eval_trainer import EvalTrainer
 
 
-def get_all_files():
+def download_batch(n):
+    server = config.UPLOAD_SERVER
+    r = requests.get(server + '/replays/list')
+    replays = r.json()
+    downloads = random.sample(replays, n)
+    files = []
+    for f in downloads:
+        r = requests.get(server + '/replays/' + f)
+        files.append(io.BytesIO(r.content))
+    return files
+
+
+def get_all_files(download=False, n=5):
+    if download:
+        return download_batch(n)
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     training_path = dir_path + '\\training'
     files = []
@@ -41,28 +61,40 @@ def get_trainer_class():
     return EvalTrainer
 
 
+def train_on_file(trainerClass, f):
+    trainerClass.start_new_file()
+    binary_converter.read_data(f, trainerClass.process_pair)
+    trainerClass.end_file()
+
+
 if __name__ == '__main__':
-    files = get_all_files()
+    files = get_all_files(download=True)
     print('training on ' + str(len(files)) + ' files')
     trainerClass = get_trainer_class()()
     counter = 0
     total_time = 0
     try:
         for file in files:
+            # file.seek(0)
             start = time.time()
             counter += 1
             try:
-                with gzip.open(file, 'rb') as f:
-                    trainerClass.start_new_file()
-                    print('running file ' + file + 'file ' + str(counter) + '/' + str(len(files)))
-                    binary_converter.read_data(f, trainerClass.process_pair)
-                    trainerClass.end_file()
+                print('running file ' + str(counter) + '/' + str(len(files)))
+                if isinstance(file, io.BytesIO):
+                    # file in memory
+                    with gzip.GzipFile(fileobj=file, mode='rb') as f:
+                        train_on_file(trainerClass=trainerClass, f=f)
+                else:
+                    # file on disk
+                    with gzip.open(file, 'rb') as f:
+                        train_on_file(trainerClass=trainerClass, f=f)
                 end = time.time()
                 difference = end - start
                 total_time += difference
                 print('trained file in ' + str(difference) + 's')
-            except FileNotFoundError:
+            except FileNotFoundError as e:
                 print('whoops file not found')
+                print(e.filename)
                 print(file)
     finally:
         print('ran through all files in ' + str(total_time / 60) + 'm')
