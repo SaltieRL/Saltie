fixed the model training to now properly compute gradients and losses

models/actor_critic/base_actor_critic.py
@@ -151,13 +151,16 @@ class BaseActorCritic(base_reinforcement.BaseReinforcement):
     def critic_network(self, input_states):
         # define policy neural network
         critic_prefix = 'critic'
-        layer1 = self.create_layer(tf.nn.relu6, input_states, 1, self.state_dim, self.network_size, critic_prefix)
+        critic_size = self.network_size
+        # four sets of actions why not! :)
+        output_size = self.num_actions #4
+        layer1 = self.create_layer(tf.nn.relu6, input_states, 1, self.state_dim, critic_size, critic_prefix)
         inner_layer = layer1
         for i in range(0, self.num_layers - 2):
-            inner_layer = self.create_layer(tf.nn.relu6, inner_layer, i + 2, self.network_size,
-                                            self.network_size, critic_prefix)
+            inner_layer = self.create_layer(tf.nn.relu6, inner_layer, i + 2, critic_size,
+                                            critic_size, critic_prefix)
         output_layer = self.create_layer(tf.nn.sigmoid, inner_layer, self.num_layers,
-                                         self.network_size, self.num_actions, critic_prefix)
+                                         critic_size, output_size, critic_prefix)
         return output_layer
 
     def get_model_name(self):
@@ -167,16 +170,16 @@ class BaseActorCritic(base_reinforcement.BaseReinforcement):
         return taken_actions
 
     def create_last_layer(self, activation_function, inner_layer, num_layers, network_size, num_actions, actor_prefix):
-
+        last_layer_name = 'last'
         if not self.action_handler.is_split_mode():
-            self.actor_last_row_layer = (self.create_layer(activation_function, inner_layer, num_layers,
+            self.actor_last_row_layer = (self.create_layer(activation_function, inner_layer, last_layer_name,
                                                            network_size, num_actions, actor_prefix))
             return self.actor_last_row_layer
 
 
         self.actor_last_row_layer = []
         for i, item in enumerate(self.action_handler.get_split_sizes()):
-            self.actor_last_row_layer.append(self.create_layer(activation_function, inner_layer, num_layers, network_size, item, actor_prefix + str(i)))
+            self.actor_last_row_layer.append(self.create_layer(activation_function, inner_layer, last_layer_name, network_size, item, actor_prefix + str(i)))
 
         last_row_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="actor_network/last_layer")
         reshaped_list = np.reshape(np.array(last_row_variables), [int(len(last_row_variables) / 2), 2])

models/actor_critic/policy_gradient.py
@@ -26,8 +26,10 @@ class PolicyGradient(BaseActorCritic):
 
 
     def create_training_op(self, logprobs, taken_actions):
-        critic_gradients, critic_loss = self.create_critic_gadients()
-        actor_gradients, actor_loss = self.create_actor_gradients(logprobs, taken_actions)
+        critic_gradients, critic_loss, critic_reg_loss = self.create_critic_gadients()
+        actor_gradients, actor_loss, actor_reg_loss = self.create_actor_gradients(logprobs, taken_actions)
+
+        tf.summary.scalar("total_reg_loss", critic_reg_loss + actor_reg_loss)
 
         return self._compute_training_op(actor_gradients, critic_gradients)
 
@@ -38,6 +40,8 @@ class PolicyGradient(BaseActorCritic):
 
         actor_reg_loss = tf.reduce_sum([tf.reduce_sum(tf.square(x)) for x in all_actor_variables],
                                        name='actor_reg_loss')
+
+        tf.summary.scalar("actor_reg_loss", actor_reg_loss)
 
 
         result = self.action_handler.run_func_on_split_tensors([logprobs,
@@ -65,7 +69,7 @@ class PolicyGradient(BaseActorCritic):
 
         tf.summary.scalar("toal_actor_loss", total_loss)
 
-        return merged_gradient_list, total_loss
+        return merged_gradient_list, total_loss, actor_reg_loss
 
 
     def create_split_actor_loss(self, logprobs, taken_actions, actor_reg_loss, advantages, actor_network_variables):
@@ -97,13 +101,15 @@ class PolicyGradient(BaseActorCritic):
         critic_reg_loss = tf.reduce_sum([tf.reduce_sum(tf.square(x)) for x in self.critic_network_variables],
                                         name='critic_reg_loss')
 
+        tf.summary.scalar("critic_reg_loss", critic_reg_loss)
+
         # compute critic gradients
         mean_square_loss = tf.reduce_mean(tf.square(self.discounted_rewards - self.estimated_values), name='mean_square_loss')
 
         critic_loss = mean_square_loss + self.reg_param * critic_reg_loss
         tf.summary.scalar("critic_loss", critic_loss)
         critic_gradients = self.optimizer.compute_gradients(critic_loss, self.critic_network_variables)
-        return (critic_gradients, critic_loss)
+        return (critic_gradients, critic_loss, critic_reg_loss)
 
     def _compute_training_op(self, actor_gradients, critic_gradients):
 