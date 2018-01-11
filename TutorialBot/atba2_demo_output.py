d345710a7320bc05d289d998c7e281595ae6458e
dtracers <dtracers@gmail.com>
2018-01-11 07:21:05 -0800

    added zeros everywhere

--- TutorialBot/atba2_demo_output.py
+++ TutorialBot/atba2_demo_output.py
@@ -28,7 +28,7 @@ class TutorialBotOutput:
         txv, tyv, tzv = local(bV,zeros3,pR)
         xv, yv, zv = pxv-txv, pyv-tyv, pzv-tzv
 
-        dT = (.5*tf.abs(ty) + 0.9*tf.abs(tx) + .34*tf.abs(tz))/1500
+        dT = (.5*tf.abs(ty) + 0.9*tf.abs(tx) + .34*tf.abs(tz))/1500.0
         tLV = bL + bV * dT
 
         x,y,z = local(tLV,pL,pR)
@@ -36,14 +36,17 @@ class TutorialBotOutput:
         r = pR[2]/U
 
         # controlls
-        throttle = bucket((y-yv*.2)/500)
-        steer = bucket(a-av/45)
-        yaw = bucket(a-av/13)
-        pitch = bucket(-i-iv/15)
-        roll = bucket(-r+rv/22)
+        throttle = bucket((y-yv*.2)/900.0)
+        steer = bucket((a-av/45.0)/2.0)
+        yaw = bucket((a-av/13.0)/2.0)
+        pitch = bucket(-i-iv/15.0)
+        roll = bucket(-r+rv/22.0)
 
         boost = tf.cast( tf.logical_and( tf.greater(.15,tf.abs(a)), tf.logical_and( tf.greater(throttle,0), tf.greater(tf.abs(.5-tf.abs(i)),.25) )) ,tf.float32)
-        powerslide = tf.cast( tf.logical_and( tf.greater(throttle*pyv,0), tf.logical_and( tf.greater(tf.abs(a-av/35),.2), tf.logical_and( tf.greater(.8,tf.abs(a-av/35)), tf.greater(xv,500) ) ) ) ,tf.float32)
+        powerslide = tf.cast( tf.logical_and( tf.greater(throttle*pyv,0.0),
+                                              tf.logical_and( tf.greater(tf.abs(a-av/35.0),.2),
+                                                              tf.logical_and( tf.greater(.8,tf.abs(a-av/35.0)),
+                                                                              tf.greater(xv,500.0) ) ) ) ,tf.float32)
 
         output = [throttle, steer, pitch, yaw, roll, jump, boost, powerslide]
 
@@ -57,8 +60,8 @@ def a3(V):
     return tf.cast(a,tf.float32)
 
 def Range180(value,pi):
-    value = value - tf.abs(value)//(2*pi) * (2*pi) * tf.sign(value)
-    value = value - tf.cast(tf.greater( tf.abs(value), pi),tf.float32) * (2*pi) * tf.sign(value)
+    value = value - tf.abs(value)//(2.0*pi) * (2.0*pi) * tf.sign(value)
+    value = value - tf.cast(tf.greater( tf.abs(value), pi),tf.float32) * (2.0*pi) * tf.sign(value)
     return value
 
 def rotate2D(x,y,ang):
@@ -70,7 +73,7 @@ def local(tL,oL,oR,Urot=True):
     L = tL-oL
     if Urot :
         pitch = oR[0]*pi/U
-        yaw = Range180(oR[1]-16384,32768)*pi/U
+        yaw = Range180(oR[1]-16384.0,32768.0)*pi/U
         roll = oR[2]*pi/U
         R = -tf.stack([pitch,yaw,roll])
     else :
@@ -92,8 +95,8 @@ def d3(A,B=[0,0,0]):
     return tf.sqrt((A[0]-B[0])**2+(A[1]-B[1])**2+(A[2]-B[2])**2)
 
 def bucket(a):
-    cond1 = tf.cast(abs(a)>.03,tf.float32)
+    cond1 = tf.cast(abs(a)> .2, tf.float32)
     result = cond1*tf.sign(a) + (1-cond1)*.5*tf.sign(a)
-    cond2 = tf.cast(abs(a)>.01,tf.float32)
+    cond2 = tf.cast(abs(a)>.07, tf.float32)
     result = cond2*result
     return result
