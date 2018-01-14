7166ea3b5e1f47b686a4cb475ed534da493b6a93
dtracers <dtracers@gmail.com>
2018-01-14 12:46:35 -0800

    potnetially fixed some bugs

--- modelHelpers/actions/action_factory.py
+++ modelHelpers/actions/action_factory.py
@@ -30,6 +30,12 @@ mixed_controls = [[('throttle', (-1, 1.5, .5), LOSS_SPARSE_CROSS), ('steer', (-1
                        [('jump', (0, 2, 1)), ('boost', (0, 2, 1)), ('handbrake', (0, 2, 1))],
                        []]
 
+no_combo = [[('throttle', (-1, 1.5, .5)), ('steer', (-1, 1.5, .5)),
+                       ('yaw', (-1, 1.5, .5)), ('pitch', (-1, 1.5, .5)), ('roll', (-1, 1.5, .5)),
+                        ('jump', (0, 2, 1)), ('boost', (0, 2, 1)), ('handbrake', (0, 2, 1))],
+                      [],
+                      []]
+
 def get_handler(split_mode=True, control_scheme=default_scheme):
     """
     Creates a handler based on the options given.
