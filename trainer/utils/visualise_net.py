4741373ffacfb7faf343e549ed66986868e20af3
hallo-doei <edwin.kanis@outlook.com>
2018-01-19 10:52:00 +0100

    Add layer neuron info visualisation

--- trainer/utils/visualise_net.py
+++ trainer/utils/visualise_net.py
@@ -1,17 +1,15 @@
 from tkinter import *
 import numpy as np
 
-above_white = 10
-left_white = 10
-highrelu = 10
+above_white = 20
+left_white = 20
+highrelu = 20
 x_spacing = 100
 y_spacing = 50
 circle_dia = 30
-
+invert_canvas = True
 
 class AutoScrollbar(Scrollbar):
-    ''' A scrollbar that hides itself if it's not needed.
-        Works only if you use the grid geometry manager '''
     def set(self, lo, hi):
         if float(lo) <= 0.0 and float(hi) >= 1.0:
             self.grid_remove()
@@ -22,7 +20,7 @@ class AutoScrollbar(Scrollbar):
 class Visualiser:
     def __init__(self):
         self.gui = Tk()
-        self.gui.geometry('600x450+300+100')
+        self.gui.geometry('600x600')
         self.gui.title("Net visualisation")
 
         # Are activations relu:
@@ -34,25 +32,55 @@ class Visualiser:
         for item in self.output:
             if len(item) > self.biggestarraylen:
                 self.biggestarraylen = len(item)
-        self.eFrame = Frame(self.gui)
+
+        self.rFrame = Frame(self.gui, width=200, height=450)
+        self.rFrame.grid(row=0, column=0)
+
+        self.eFrame = Frame(self.rFrame, width=200, height=250)
         self.edit_stuff()
         self.eFrame.grid(row=0, column=0)
 
-        self.cFrame = Frame(self.gui, width=600, height=450)
+        self.iFrame = Frame(self.rFrame, width=200, height=200)
+        self.info_stuff()
+        self.iFrame.grid(row=1, column=0)
+
+        self.cFrame = Frame(self.gui, width=400, height=450)
         self.canvas_stuff()
         self.cFrame.grid(row=0, column=1, sticky='w')
         mainloop()
 
-    def create_circle(self, x0, y0, activation, relu):
+    def edit_stuff(self):
+        self.relu_number = Spinbox(self.eFrame, from_=1, to=100)
+        self.relu_number.grid(row=1, column=0)
+        Label(self.eFrame, text="Customisation part is still wip").grid(row=0, column=0)
+
+    def info_stuff(self):
+        self.info_activation = StringVar()
+        activation_label = Label(self.iFrame, textvariable=self.info_activation)
+        activation_label.grid(row=0, column=0)
+
+    def info_update_neuron(self, layer, neuron):
+        self.info_activation.set("Layer:" + str(layer) + "\n" + "Neuron:" + str(neuron))
+
+    def create_circle(self, x0, y0, activation, relu, layer, neuron):
+        if invert_canvas:
+            x0, y0 = y0, x0
         if relu:
             activation = activation if activation <= highrelu else highrelu
-            rgb = int(-1 * (activation - highrelu) * 25.5)
+            rgb = int(-1 * (activation - highrelu) * 255 / highrelu)
         else:
             rgb = int(-1 * (activation - 1) * 255)
         hexcolor = "#{:02x}{:02x}{:02x}".format(rgb, rgb, rgb)
-        self.canvas.create_oval(x0, y0, x0 + circle_dia, y0 + circle_dia, fill=hexcolor, tags='circle')
+        tag = str(layer) + ";" + str(neuron)
+        self.canvas.create_oval(x0, y0, x0 + circle_dia, y0 + circle_dia, fill=hexcolor, tags=tag)
+
+        def handler(event, l=layer, n=neuron):
+            self.info_update_neuron(l, n)
+        self.canvas.tag_bind(tag, "<Motion>", handler)
 
     def create_line(self, x0, y0, x1, y1):
+        if invert_canvas:
+            x0, y0, x1, y1 = y0, x0, y1, x1
         half = .5 * circle_dia
         self.canvas.create_line(x0 + half, y0 + half, x1 + half, y1 + half, tags='line')
 
@@ -62,17 +90,16 @@ class Visualiser:
         x = layer * x_spacing + left_white
         y = (self.biggestarraylen - len(activations)) * y_spacing * .5 + above_white
         this_layer = list()
+        neuron = 0
         for i in activations:
             this_layer.append([x, y])
             if layer != 0:
                 for n in self.last_layer:
                     self.create_line(n[0], n[1], x, y)
-            self.create_circle(x, y, i, relu)
+            self.create_circle(x, y, i, relu, layer, neuron)
             y += y_spacing
+            neuron += 1
         self.last_layer = this_layer
-
-    def edit_stuff(self):
-        print("Test")
 
     def canvas_stuff(self):
         vbar = AutoScrollbar(self.cFrame, orient='vertical')
@@ -80,9 +107,7 @@ class Visualiser:
         vbar.grid(row=0, column=1, sticky='ns')
         hbar.grid(row=1, column=0, sticky='we')
 
-        max_width = left_white + (len(self.output) - 1) * x_spacing + circle_dia + left_white
-        max_height = above_white + (self.biggestarraylen - 1) * y_spacing + circle_dia + above_white
-        self.canvas = Canvas(self.cFrame, width=550, height=400, xscrollcommand=hbar.set,
+        self.canvas = Canvas(self.cFrame, width=400, height=450, xscrollcommand=hbar.set,
                              yscrollcommand=vbar.set)
         self.canvas.grid(row=0, column=0)
         for i in range(len(self.output)):
@@ -95,7 +120,7 @@ class Visualiser:
         self.cFrame.rowconfigure(0, weight=1)
         self.cFrame.columnconfigure(0, weight=1)
         # Bind events to the Canvas
-        self.canvas.bind('<ButtonPress-1>', self.move_from)
+        self.canvas.bind('<Button-1>', self.move_from)
         self.canvas.bind('<B1-Motion>', self.move_to)
         self.canvas.bind('<MouseWheel>', self.wheel)
         self.imscale = 1.0
@@ -104,15 +129,12 @@ class Visualiser:
         self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def move_from(self, event):
-        ''' Remember previous coordinates for scrolling with the mouse '''
         self.canvas.scan_mark(event.x, event.y)
 
     def move_to(self, event):
-        ''' Drag (move) canvas to the new position '''
         self.canvas.scan_dragto(event.x, event.y, gain=1)
 
     def wheel(self, event):
-        ''' Zoom with mouse wheel '''
         scale = 1.0
         # Respond to Linux (event.num) or Windows (event.delta) wheel event
         if event.num == 5 or event.delta == -120:
