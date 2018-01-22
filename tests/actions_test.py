db02c4e71f0a4b625758b2e0e36c75ea9c7e030c
dtracers <dtracers@gmail.com>
2018-01-22 13:56:35 -0800

    added more to the test

--- tests/actions_test.py
+++ tests/actions_test.py
@@ -1,4 +1,4 @@
-from modelHelpers.actions import action_handler, dynamic_action_handler, action_factory
+from modelHelpers.actions import action_factory
 import tensorflow as tf
 import numpy as np
 
@@ -95,7 +95,7 @@ def test2():
 
 def test3():
     handler = action_factory.get_handler(False)
-    dynamic_handler = action_factory.get_handler(True, dynamic_action_handler.super_split_scheme)
+    dynamic_handler = action_factory.get_handler(True, action_factory.regression_controls)
 
     session = tf.Session(config=tf.ConfigProto(
         device_count={'GPU': 0}
@@ -109,20 +109,23 @@ def test3():
                       [-0.2, -0.3,  0.2,  0.3, 0.0, 1.0, 0.0, 0.0],
                       [ 1.0, -0.3,  0.2,  0.3, 0.0, 0.0, 1.0, 0.0],
                       [-1.0, -0.3,  0.2,  0.3, 0.0, 0.0, 0.0, 1.0],
-                      [-0.25, -0.75,  0.25,  0.75, 0.0, 0.0, 0.0, 0.0]])
+                      [-0.25, -0.75,  0.25,  0.75, 0.0, 0.0, 0.0, 0.0],
+                      [-0.25, -0.75,  0.25,  0.75, 0.0, 0.0, 1.0, 1.0],
+                      [-0.25, -0.75,  0.25,  0.75, 0.0, 1.0, 0.0, 1.0],
+                      [-0.25, -0.75,  0.25,  0.75, 0.0, 1.0, 1.0, 0.0]])
 
     #t, y, p, r,
     real_action = tf.Variable(input, dtype=tf.float32)
 
     action_index = dynamic_handler.create_action_indexes_graph(real_action)
-    back_again = dynamic_handler.create_tensorflow_controller_from_selection(tf.transpose(action_index), batch_size=9)
+    back_again = dynamic_handler.create_tensorflow_controller_from_selection(tf.transpose(action_index), batch_size=len(input))
 
     init = tf.global_variables_initializer()
     session.run(init)
 
     indexes, dynamic_results = session.run([action_index, back_again])
 
-    for index in range(9):
+    for index in range(len(input)):
         row = input[index]
         print('blank row')
         #     print('input row    ', np.array(row, dtype=np.float32))
@@ -132,7 +135,7 @@ def test3():
 
         print('and back again')
         print('correct answer', row)
-        print('numpy result', handler.create_controller_from_selection(action_index))
+        print('numpy result', dynamic_handler.create_controller_from_selection(indexes[index]))
         # purposely using the working result
         print('dynamic result', dynamic_results[index])
 
