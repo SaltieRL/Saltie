b5ac5f92b515e456d414579cd299e6c55cf1f61c
hallo-doei <edwin.kanis@outlook.com>
2018-02-05 16:16:24 +0100

    fixed bug and added logo

--- GUI_trainer.py
+++ GUI_trainer.py
@@ -10,19 +10,21 @@ class TrainerGUI(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.parent.title("Trainer GUI")
+        self.parent.iconbitmap(default="images" + os.sep + "Saltie_logo.ico")
 
         self.trainer_path = tk.StringVar()
 
-        tk.Label(self, text="Trainer path: ").grid(row=0, column=0)
-        tk.Entry(self, textvariable=self.trainer_path).grid(row=0, column=1, sticky="ew")
-        tk.Button(self, text="Select file", command=self.change_trainer_path).grid(row=0, column=2)
+        ttk.Label(self, text="Trainer path: ", anchor="e", justify="right").grid(row=0, column=0, padx=(0, 5))
+        ttk.Entry(self, textvariable=self.trainer_path, state="readonly").grid(row=0, column=1, sticky="ew")
+        ttk.Button(self, text="Select file", command=self.change_trainer_path).grid(row=0, column=2, padx=(5, 0))
 
-        tk.Label(self, text="Config: ").grid(row=1, column=0)
+        ttk.Label(self, text="Config: ", anchor="e", justify="right").grid(row=1, column=0, padx=(0, 5))
         self.config_options = ttk.Combobox(self, state="readonly")
         self.config_options.bind("<<ComboboxSelected>>", self.change_config)
         self.config_options.grid(row=1, column=1, sticky="ew")
-        self.config_button = tk.Button(self, text="Add config", command=self.add_config_option, state="disabled")
-        self.config_button.grid(row=1, column=2)
+        self.config_button = ttk.Button(self, text="Add config", command=self.add_config_option, state="disabled")
+        self.config_button.grid(row=1, column=2, padx=(5, 0))
 
     def initialise_custom_config(self):
         self.custom_options = tk.Frame(self)
@@ -33,16 +35,17 @@ class TrainerGUI(tk.Frame):
                 continue
             header_frame = tk.Frame(self.custom_options)
             header_frame.grid(row=0, column=header_index)
-            tk.Label(header_frame, text=header.name).grid(row=0, column=0, columnspan=2)
+            ttk.Label(header_frame, text=header.name).grid(row=0, column=0, columnspan=2)
             self.trainer_config[header.name] = {}
             for i, parameter in enumerate(header.values):
-                tk.Label(header_frame, text=parameter.name).grid(row=i + 1, column=0)
+                ttk.Label(header_frame, text=parameter.name).grid(row=i + 1, column=0)
+                big = 20000000
                 if parameter.type == int:
                     self.trainer_config[header.name][parameter.name] = tk.IntVar()
-                    tk.Spinbox(header_frame, textvariable=self.trainer_config[header.name][parameter.name]).grid(row=i + 1, column=1)
+                    tk.Spinbox(header_frame, textvariable=self.trainer_config[header.name][parameter.name], from_=0, to=big).grid(row=i + 1, column=1)
                 elif parameter.type == float:
                     self.trainer_config[header.name][parameter.name] = tk.DoubleVar()
-                    tk.Spinbox(header_frame, textvariable=self.trainer_config[header.name][parameter.name]).grid(row=i + 1, column=1)
+                    tk.Spinbox(header_frame, textvariable=self.trainer_config[header.name][parameter.name], from_=0, to=big, increment=.01).grid(row=i + 1, column=1)
                 elif parameter.type == bool:
                     print("Gotta add boolean thing")
                     self.trainer_config[header.name][parameter.name] = tk.BooleanVar()
