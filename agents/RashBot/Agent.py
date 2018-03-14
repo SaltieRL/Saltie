4434d204516de728516b70b63e3c8e843f24ac96
Marvin <35970829+RLMarvin@users.noreply.github.com>
2018-03-14 17:58:37 +0000

    Adding RashBot

---
+++ agents/RashBot/Agent.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+folder = os.path.dirname(os.path.realpath(__file__))
+if folder not in sys.path:
+    sys.path.append(folder)
+
+from Bot import Process
+
+
+class Agent:
+    def __init__(self, name, team, index):
+        self.index = index
+
+    def get_output_vector(self, game):
+        return Process(self, game)
+
+
+class agent:
+    def __init__(self, index):
+        self.index = index
+
+    def get_output_vector(self, game):
+        return Process(self, game.GameTickPacket, 2)
