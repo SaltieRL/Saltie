fb70813af326f2414228169cecd0001dad47e62e
Dominik Schmid <dominikschmid93@gmail.com>
2018-03-30 15:43:51 +1100

    Put tests into functions.

--- bot_code/conversions/integration_test.py
+++ bot_code/conversions/integration_test.py
@@ -16,57 +16,82 @@ from bot_code.conversions import output_formatter
 from bot_code.conversions import binary_converter
 from game_data_struct import GameTickPacket
 
-# pre-test: This used to fail due to mutated external state in conversion functions.
-game_tick_packet = GameTickPacket()
-input_formatter = InputFormatter(1,0)
-array1 = input_formatter.create_input_array(game_tick_packet, passed_time=0.0)
-array2 = input_formatter.create_input_array(game_tick_packet, passed_time=0.0)
-assert all(x==y for x,y in zip(array1, array2))
+
+def test_create_input_array_is_idemodent():
+    # pre-test: This used to fail due to mutated external state in conversion functions.
+    game_tick_packet = GameTickPacket()
+    input_formatter = InputFormatter(1,0)
+    array1 = input_formatter.create_input_array(game_tick_packet, passed_time=0.0)
+    array2 = input_formatter.create_input_array(game_tick_packet, passed_time=0.0)
+    assert all(x==y for x,y in zip(array1, array2))
 
 
+def write_test_data_to_file(replay_file):
+    # prepare data
+    team = 1
+    player_index = 2
+    bot_hash = 41
+    is_eval = False
+    game_tick_packet = GameTickPacket()
+    game_tick_packet.gamecars[player_index].team = 1
+    game_tick_packet.gamecars[player_index].boost = 30
+    game_tick_packet.gamecars[player_index].Location.X = 31.
+    input_formatter = InputFormatter(team, player_index)
+    test_data_pairs = [
+        (
+            input_formatter.create_input_array(game_tick_packet, passed_time=42.0),
+            [0.0, 43.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ),
+        (
+            input_formatter.create_input_array(game_tick_packet, passed_time=52.0),
+            [0.0, 53.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        ),
+    ]
 
-# def write_test_data_to_file(replay_file):
-#     # prepare data
-#     team = 1
-#     player_index = 1
-#     bot_hash = 41
-#     is_eval = False
-#     game_tick_packet = GameTickPacket()
-#     game_tick_packet.gamecars[1].team = 1
-#     game_tick_packet.gamecars[1].boost = 30
-#     input_formatter = InputFormatter(team, player_index)
-#     test_data_pairs = [
-#         (
-#             input_formatter.create_input_array(game_tick_packet, passed_time=42.0),
-#             [0.0, 43.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-#         ),
-#         (
-#             input_formatter.create_input_array(game_tick_packet, passed_time=52.),
-#             [0.0, 53.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-#         ),
-#     ]
+    # Note: This is API is fragile. Any mistake in ordering or ommission will throw things out of whack.
 
-#     # Note: This is API is fragile as fuck. Any mistake in ordering or ommission will throw everything out of whack.
+    # Write header
+    binary_converter.write_version_info(replay_file, binary_converter.get_latest_file_version())
+    binary_converter.write_bot_hash(replay_file, bot_hash)
+    binary_converter.write_is_eval(replay_file, is_eval)
 
-#     # Write header
-#     binary_converter.write_version_info(replay_file, binary_converter.get_latest_file_version())
-#     binary_converter.write_bot_hash(replay_file, bot_hash)
-#     binary_converter.write_is_eval(replay_file, is_eval)
+    # Write body
+    for state_array, output_vector_array in test_data_pairs:
+        binary_converter.write_array_to_file(replay_file, state_array)
+        binary_converter.write_array_to_file(replay_file, output_vector_array)
 
-#     # Write body
-#     for state_array, output_vector_array in test_data_pairs:
-#         binary_converter.write_array_to_file(replay_file, state_array)
-#         binary_converter.write_array_to_file(replay_file, output_vector_array)
+def read_from_file_and_assert(replay_file):
+    tuples = list(binary_converter.iterate_data(replay_file))
+    # print([len(x) for x in tuples])
+    for i, (state_array, output_vector_array, pair_number) in enumerate(tuples):
+        # state = output_formatter.get_advanced_state(state_array)
+        state = output_formatter.get_basic_state(state_array)
 
-# def read_from_file_and_assert(replay_file):
-#     tuples = list(binary_converter.iterate_data(replay_file))
-#     for state_array, output_vector_array in tuples:
-#         print (t)
+        # Test things that should be the same between the ticks
+        # print (state_array)
+        # assert state.car_info.Boost == 30
+        # assert state.car_info.Location.X == 31.0
+
+        # Test things that are different between the ticks
+        if i == 0:
+            # assert state.passed_time == 42.
+            assert output_vector_array[1] == 43.
+        elif i == 1:
+            # assert state.passed_time == 51.
+            assert output_vector_array[1] == 53.
+        else:
+            assert False, 'only expected two ticks worth of data'
+
+        # print(state.car_info.Team)  # Debatable whether this should be 0 or 1 as we never asked for the field to be rotated.
 
 
-# with tempfile.TemporaryFile() as replay_file:
-#     write_test_data_to_file(replay_file)
-#     replay_file.seek(0)
-#     read_from_file_and_assert(replay_file)
+def test_read_write_preserves_data():
+    with tempfile.TemporaryFile() as replay_file:
+        write_test_data_to_file(replay_file)
+        replay_file.seek(0)
+        read_from_file_and_assert(replay_file)
 
+
+test_create_input_array_is_idemodent()
+test_read_write_preserves_data()
 print (' === ALL TESTS PASSED === ')
