7e30c47856b5627e61eb907191531eb3c82547bf
dtracers <dtracers@gmail.com>
2018-07-08 22:59:12 -0600

    added lstm fields for statefulness

--- legacy/legacy_keras_lstm.py
+++ legacy/legacy_keras_lstm.py
@@ -5,6 +5,11 @@ import tensorflow as tf
 
 class LegacyKerasLstm(LegacyKerasModel):
     lstm_state = None
+    prediction_mode = False
+
+    def __init__(self, prediction_mode=False):
+        super().__init__()
+        self.prediction_mode = prediction_mode
 
     def create_input_layer(self, input_placeholder: BaseInputFormatter):
         """Creates keras model"""
@@ -14,6 +19,7 @@ class LegacyKerasLstm(LegacyKerasModel):
 
     def create_hidden_layers(self):
         model = self.model
-        lstm = tf.keras.layers.LSTM(units=512, kernel_regularizer=self.kernel_regularizer, recurrent_dropout=0.1, return_sequences=True)
+        lstm = tf.keras.layers.LSTM(units=512, kernel_regularizer=self.kernel_regularizer, recurrent_dropout=0.1,
+                                    return_sequences=True, stateful=self.prediction_mode)
         model.add(lstm)
         super().create_hidden_layers()
