234351f4f356ce957598cbd93ea7adfebe5ca9ca
dtracers <dtracers@gmail.com>
2018-07-09 02:47:03 -0600

    added a way for models to return layers and take in layers

--- framework/model/base_model.py
+++ framework/model/base_model.py
@@ -18,16 +18,20 @@ class BaseModel:
         self.logger = get_logger(str(type(self).__name__))
 
     def create_input_layer(self, input_formatter: BaseInputFormatter):
-        """Creates the input layer of the model, takes in feeding dicts"""
+        """Creates the input layer of the model, takes in feeding dicts
+        :return The layer you want to feed into hidden layers"""
         pass
 
-    def create_hidden_layers(self):
-        """Creates the internal hidden layers if needed"""
+    def create_hidden_layers(self, input_layer=None):
+        """Creates the internal hidden layers if needed.
+        :param input_layer: The previous input layer, can be None
+        :return The layer you want to feed into the output layer"""
         pass
 
-    def create_output_layer(self, formatter: BaseOutputFormatter):
+    def create_output_layer(self, formatter: BaseOutputFormatter, hidden_layer=None):
         """Creates the output layer of the model.
-        :param formatter:
+        :param formatter: Formats the output
+        :param hidden_layer: The previous hidden layer, can be None
         :return The output layer of the model"""
 
     def finalize_model(self):
