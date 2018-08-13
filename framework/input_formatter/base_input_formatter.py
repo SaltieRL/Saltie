2b3d64283891c8a3ecc1a0cdb5ecd2b085a6f398
dtracers <dtracers@gmail.com>
2018-08-12 23:49:38 -0600

    added some documentation

--- framework/input_formatter/base_input_formatter.py
+++ framework/input_formatter/base_input_formatter.py
@@ -2,18 +2,25 @@ from rlbot.utils.logging_utils import get_logger
 
 
 class BaseInputFormatter:
+    """
+    A skeleton class for all other InputFormatters.
+    """
 
     logger = None
 
     def __init__(self):
         self.logger = get_logger(str(type(self).__name__))
 
-    def create_input_array(self, input_array, batch_size=1):
+    def create_input_array(self, input_data, batch_size=1):
         """
-        Creates an array for the model from the game_tick_packet
-        :return: A massive array representing that packet
+        Creates an array for the model from the input data.
+        :param input_data: Can be any format that the formatter understands.
+        :param batch_size: Specifies the batch size of the input data if that is batched.
+        Also the batch size what this formatter returns.
+        :return: An array that can then be directly fed into a model.
+        This can be a python/numpy array or it can be tensorflow/pytorch data.
         """
-        return input_array
+        return input_data
 
     def get_input_state_dimension(self):
         """
--- framework/input_formatter/host_input_formatter.py
+++ framework/input_formatter/host_input_formatter.py
@@ -11,6 +11,15 @@ class HostInputFormatter(BaseInputFormatter):
         self.input_formatter = input_formatter
 
     def create_input_array(self, input_array, batch_size=1):
+        """
+        By default just calls the input_formatter this holds.
+        Can be used to chain together formatting.
+        :param input_array: Can be any format that the formatter understands.
+        :param batch_size: Specifies the batch size of the input data if that is batched.
+        Also the batch size what this formatter returns.
+        :return: An array that can then be directly fed into a model.
+        This can be a python/numpy array or it can be tensorflow/pytorch data.
+        """
         converted_input_array = self.input_formatter.create_input_array(input_array)
         return converted_input_array
 
