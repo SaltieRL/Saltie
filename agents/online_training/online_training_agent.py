2967ee6b1ac782fcf217ab0c71a320a10281b2eb
dtracers <dtracers@gmail.com>
2018-09-09 13:09:55 -0600

    added train step to training agent

--- agents/online_training/online_training_agent.py
+++ agents/online_training/online_training_agent.py
@@ -5,10 +5,11 @@ from swarm_trainer.reward_memory import BaseRewardMemory
 
 
 class OnlineTrainingAgent(BaseModelAgent):
+
     def __init__(self, name, team, index):
         super().__init__(name, team, index)
-        self.action_history = BaseRewardMemory()
 
     def predict(self, packet: GameTickPacket):
         result = self.model_holder.predict(packet)
+        self.model_holder.train_step(packet, result)
         return result
