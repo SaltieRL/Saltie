d84801c4722754417ef2b032e7a042283a4d0bb5
dtracers <dtracers@gmail.com>
2018-09-17 00:06:10 -0600

    started work on adding features

--- examples/replays/shots/shot_input_formatter.py
+++ examples/replays/shots/shot_input_formatter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from framework.input_formatter.base_input_formatter import BaseInputFormatter
 from framework.replay.replay_format import GeneratedHit
 
@@ -12,10 +13,31 @@ class ShotInputFormatter(BaseInputFormatter):
         hit = input_data.get_hit()
         frame = hit.frame_number
         df = input_data.get_replay().get_pandas()
-        df.set_index(('index',), inplace=True)
         index = df.index
         hit_frame = df.loc[frame]
 
-        result = input_data.get_replay().get_pandas()
-        index = input_data.get_replay().get_pandas().loc[frame].index
+        new_frame = hit_frame['ball']
+
+        proto = input_data.get_replay().get_proto()
+        hit_player = hit.player_id
+        blue_team = []
+        orange_team = []
+        for player in proto.players:
+            if player.is_orange:
+                orange_team.append(player)
+            else:
+                blue_team.append(player)
+
+
         return result
+
+    def get_speed(self, frame):
+        return np.sqrt(frame['vel_x']**2 + frame['vel_y']**2 + frame['vel_z']**2)
+
+    def get_distance_from_goal(self, frame, player):
+
+
+    def get_player_data(self, frame):
+        return [frame['pos_x'], frame['pos_y'], frame['pos_z'],
+                frame['rot_x'], frame['rot_y'], frame['rot_z'],
+                frame['vel_x'], frame['vel_y'], frame['vel_z']]
