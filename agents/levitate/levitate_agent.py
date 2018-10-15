ba1f4c9ad9baa507819e79dbc3ce663e2f0af579
dtracers <dtracers@gmail.com>
2018-10-14 21:04:21 -0600

    added code so taht anyone could be a custom bot teacher

--- agents/levitate/levitate_agent.py
+++ agents/levitate/levitate_agent.py
@@ -22,34 +22,21 @@
 
 import os
 import sys
+
+from agents.swarm.teacher_agent import TeacherAgent
+
 path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, path)  # this is for first process imports
 
-from agents.swarm.swarm_agent import SwarmAgent
 from examples.levi.output_formatter import LeviOutputFormatter
 from examples.levi.input_formatter import LeviInputFormatter
-from rlbot.agents.base_agent import SimpleControllerState, BaseAgent
-from rlbot.utils.class_importer import ExternalClassWrapper
 
 
-class LeviAgent(SwarmAgent):
+class LeviAgent(TeacherAgent):
     def __init__(self, name, team, index):
         super().__init__(name, team, index)
         import torch
         self.torch = torch
-        self.teacher = ExternalClassWrapper(
-            path + "//teachers//skybot//Skybot//Tournament editions//SkyBot_Tournament_2_edition.py",
-            BaseAgent).get_loaded_class()(name, team, index)
-
-        self.empty_controller = SimpleControllerState()
-
-    def initialize_agent(self):
-        super().initialize_agent()
-        self.teacher._set_renderer(self.renderer)
-        self.teacher.initialize_agent()
-
-    def get_manager_path(self):
-        return path + "//examples//levi//torch_manager"
 
     def create_input_formatter(self):
         return LeviInputFormatter(self.team, self.index)
@@ -57,31 +44,9 @@ class LeviAgent(SwarmAgent):
     def create_output_formatter(self):
         return LeviOutputFormatter(self.index)
 
-    def get_output(self, packet):
-        """
-        Predicts an output given the input
-        :param packet: The game_tick_packet
-        :return:
-        """
-        if not packet.game_info.is_round_active:
-            return self.empty_controller
-        if packet.game_cars[self.index].is_demolished:
-            return self.empty_controller
-
-        arr = self.input_formatter.create_input_array([packet], batch_size=1)
-
-        teacher_output = self.teacher.get_output(packet)
-        teacher_output = self.output_formatter.format_numpy_output(teacher_output, packet)
-
-        assert (arr[0].shape == (1, 3, 9))
-        assert (arr[1].shape == (1, 5))
-        assert (teacher_output.shape == (1, 9))
-
-        self.game_memory.append(arr, teacher_output)
-
+    def advanced_step(self, arr, teacher_output):
         arr = [self.torch.from_numpy(x).float() for x in arr]
 
         with self.torch.no_grad():
             output = self.model.forward(*arr)
-
-        return self.output_formatter.format_model_output(output, packet, batch_size=1)[0]
+        return output
--- agents/swarm/swarm_agent.py
+++ agents/swarm/swarm_agent.py
@@ -1,8 +1,13 @@
+import os
+
 from rlbot.botmanager.helper_process_request import HelperProcessRequest
-from rlbot.agents.base_agent import BaseAgent
+from rlbot.agents.base_agent import BaseAgent, BOT_CONFIG_AGENT_HEADER
+from rlbot.parsing.custom_config import ConfigHeader, ConfigObject
 from rlbot.utils.logging_utils import get_logger
 from framework.utils import get_repo_directory
 import sys
+
+path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class SwarmAgent(BaseAgent):
@@ -19,6 +24,7 @@ class SwarmAgent(BaseAgent):
     def __init__(self, name, team, index):
         super().__init__(name, team, index)
         self.logger = get_logger(name)
+        self.manager_path = None
         sys.path.insert(0, get_repo_directory())  # this is for separate process imports
 
     def get_helper_process_request(self) -> HelperProcessRequest:
@@ -30,8 +36,11 @@ class SwarmAgent(BaseAgent):
         self.pipe, request.pipe = Pipe(False)
         return request
 
+    def load_config(self, config_object_header: ConfigHeader):
+        self.manager_path = config_object_header.get_string('manager_path')
+
     def get_manager_path(self):
-        raise NotImplementedError
+        return os.path.join(path, self.manager_path)
 
     def create_input_formatter(self):
         raise NotImplementedError
@@ -45,5 +54,9 @@ class SwarmAgent(BaseAgent):
         self.output_formatter = self.create_output_formatter()
         self.game_memory = self.pipe.recv()
 
-        self.optimizer = self.torch.optim.Adamax(self.model.parameters())
-        self.loss_function = self.torch.nn.L1Loss()
+    @staticmethod
+    def create_agent_configurations(config: ConfigObject):
+        super(SwarmAgent, SwarmAgent).create_agent_configurations(config)
+        params = config.get_header(BOT_CONFIG_AGENT_HEADER)
+        params.add_value('manager_path', str, default=os.path.join('examples', 'levi', 'torch_manager'),
+                         description='Path to the manager bot')
