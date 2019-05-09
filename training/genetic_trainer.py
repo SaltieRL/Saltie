be66c335e619b33196cd93b5cf88a95ed4e105c2
Hytak <lhc.holten@gmail.com>
2019-05-09 19:09:07 +0200

    Implemented a brute force algorithm

--- training/genetic_trainer.py
+++ training/genetic_trainer.py
@@ -5,7 +5,7 @@ from training.linkuru_playlist import make_default_playlist
 
 from rlbot.matchcomms.common_uses.set_attributes_message import make_set_attributes_message
 from rlbot.matchcomms.common_uses.reply import send_and_wait_for_replies
-from rlbot.training.training import Grade
+from rlbot.training.training import Grade, Pass
 from rlbot.utils.logging_utils import get_logger
 from rlbot.setup_manager import setup_manager_context
 
@@ -17,11 +17,11 @@ from torch.nn import Module
 import io
 from multiprocessing.reduction import ForkingPickler
 import pickle
+import torch
 
 
 def create_on_briefing(send_model: Module) -> Callable:
     def on_briefing(self: TrainingExercise) -> Optional[Grade]:
-        del self._matchcomms
         buf = io.BytesIO()
         ForkingPickler(buf, pickle.HIGHEST_PROTOCOL).dump(send_model)
         _ = send_and_wait_for_replies(self.get_matchcomms(), [
@@ -36,10 +36,15 @@ if __name__ == '__main__':
 
     model = SymmetricModel()
     model.share_memory()
-    playlist = make_default_playlist(create_on_briefing(model))[2:4]
+    playlist = make_default_playlist(create_on_briefing(model))[0:1]
 
     with setup_manager_context() as setup_manager:
-        for i in range(5):
-            result = list(run_playlist(playlist, setup_manager=setup_manager))
+        while True:
+            model.load_state_dict(SymmetricModel().state_dict())
 
+            result = next(run_playlist(playlist, setup_manager=setup_manager))
             logger.info(result)
+
+            if isinstance(result.grade, Pass):
+                torch.save(model.state_dict(), f'exercise_{result.reproduction_info.playlist_index}.mdl')
+                break
